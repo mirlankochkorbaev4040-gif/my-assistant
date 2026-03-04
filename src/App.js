890486813939a775ee615a12332674b23f116832
mirlankochkorbaev4040-gif <mirlankochkorbaev4040@gmail.com>
2026-03-04 21:37:33 +0600

    first version

---
+++ src/App.js
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div style={{textAlign: "center", marginTop: "100px"}}>
+      <h1>Мой первый сайт 🚀</h1>
+      <p>Создан Mirlan</p>
+    </div>
+  );
+}
+
+export default App;
